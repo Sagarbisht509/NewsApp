c738c33eca053584c257ea67a38337f949f35908
sagar <sagarbisht509@gmail.com>
2023-05-13 01:51:48 +0530

    implementing adapter for all fragments

--- app/src/main/java/com/example/newsapp/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/newsapp/adapters/NewsAdapter.kt
@@ -1,25 +1,71 @@
 package com.example.newsapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.newsapp.databinding.NewsItemBinding
+import com.example.newsapp.models.Article
 
-class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+class NewsAdapter(
+    private val onArticleClicked: (Article) -> Unit,
+    private val onSaveArticleClicked: (Article) -> Unit,
+    private val from: String
+) :
+    RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
-    inner class ArticleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        
+    inner class ArticleViewHolder(private val binding: NewsItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+            binding.tvTitle.text = article.title
+            binding.tvTitle.text = article.title
+            binding.tvDescription.text = article.description
+            Glide.with(binding.root.context).load(article.urlToImage).into(binding.newsImage)
+
+            binding.root.setOnClickListener {
+                onArticleClicked(article)
+            }
+
+            if (from == "saveFragment") {
+                binding.saveBtn.visibility = View.GONE
+            }
+
+            binding.saveBtn.setOnClickListener {
+                if (from != "saveFragment") {
+                    onSaveArticleClicked(article)
+                }
+            }
+        }
     }
+
+    private val differCallBack = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallBack)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val binding = NewsItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ArticleViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        article?.let {
+            holder.bind(it)
+        }
     }
 }
