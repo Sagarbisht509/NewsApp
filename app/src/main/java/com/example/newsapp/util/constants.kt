28a4fcf9799832337ba939f21a885b23f847e228
sagar <sagarbisht509@gmail.com>
2023-05-13 01:54:53 +0530

    initializing constants

--- app/src/main/java/com/example/newsapp/util/constants.kt
+++ app/src/main/java/com/example/newsapp/util/constants.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp.util
 
-const val BASE_URL = "https://newsapi.org/v2/everything"
+const val BASE_URL = "https://newsapi.org"
 const val API_KEY = "6b8c6f02a19f4cda9f6860473d9e0a72"
 const val TABLE_NAME = "articles"
 const val DATABASE_NAME = "articleDB"
+const val SEARCH_NEWS_TIME_DELAY = 500L
